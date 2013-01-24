df9c248c7c5fcbad3b7255473945f5fcfcb0e515
Nicolas Damiens <nicolas@damiens.info>
2013-01-24 10:06:55 +0000

    modifications pour 2013
    git-svn-id: svn+ssh://oshu.picardie-nature.org/home/svn/ecovolontaires@20 329975d2-553d-4616-a718-f8f2ca88aa4d

--- trunk/inscription/models.py
+++ trunk/inscription/models.py
@@ -30,6 +30,14 @@ class Fiche(models.Model):
 		('R', 'Rouge'),
 		('N', 'Noir')
 	)
+	CHOIX_TSHIRT = (
+		('S','Taille S'),
+		('M','Taille M'),
+		('L','Taille L'),
+		('XL','Taille XL'),
+		('XXL', 'Taille XXL')
+	)
+
 	date_inscription = models.DateTimeField("Date d'inscription", default=datetime.now, blank=True)
 	nom = models.CharField("Nom", max_length=200)
 	prenom = models.CharField("Prénom", max_length=200)
@@ -41,28 +49,27 @@ class Fiche(models.Model):
 	participation_csfs = models.BooleanField("Centre de sauvegarde")
 	participation_protection = models.BooleanField("Protection des phoques")
 
-	dispo_02_06_au_09_06 = models.BooleanField("Disponible du 02-06 au 09-06")
-	dispo_09_06_au_16_06 = models.BooleanField("Disponible du 09-06 au 16-06")
-	dispo_16_06_au_23_06 = models.BooleanField("Disponible du 16-06 au 23-06")
-	dispo_23_06_au_30_06 = models.BooleanField("Disponible du 23-06 au 30-06")
-	dispo_30_06_au_07_07 = models.BooleanField("Disponible du 30-06 au 07-07")
-	dispo_07_07_au_14_07 = models.BooleanField("Disponible du 07-07 au 14-07")
-	dispo_14_07_au_21_07 = models.BooleanField("Disponible du 14-07 au 21-07")
-	dispo_21_07_au_28_07 = models.BooleanField("Disponible du 21-07 au 28-07")
-	dispo_28_07_au_04_08 = models.BooleanField("Disponible du 28-07 au 04-08")
-	dispo_04_08_au_11_08 = models.BooleanField("Disponible du 04-08 au 11-08")
-	dispo_11_08_au_18_08 = models.BooleanField("Disponible du 11-08 au 18-08")
-	dispo_18_08_au_25_08 = models.BooleanField("Disponible du 18-08 au 25-08")
-	dispo_25_08_au_01_09 = models.BooleanField("Disponible du 25-08 au 01-09")
-	dispo_01_09_au_08_09 = models.BooleanField("Disponible du 01-09 au 08-09")
-	dispo_08_09_au_15_09 = models.BooleanField("Disponible du 08-09 au 15-09")
-	dispo_15_09_au_22_09 = models.BooleanField("Disponible du 15-09 au 22-09")
-	dispo_22_09_au_29_09 = models.BooleanField("Disponible du 22-09 au 29-09")
-	dispo_29_09_au_06_10 = models.BooleanField("Disponible du 29-09 au 06-10")
-	dispo_06_10_au_13_10 = models.BooleanField("Disponible du 06-10 au 13-10")
-	dispo_13_10_au_20_10 = models.BooleanField("Disponible du 13-10 au 20-10")
-	dispo_20_10_au_27_10 = models.BooleanField("Disponible du 20-10 au 27-10")
-	dispo_27_10_au_03_11 = models.BooleanField("Disponible du 27-10 au 03-11")
+	dispo_08_06_au_15_06 = models.BooleanField("Disponible du 08-06 au 15-06")
+	dispo_15_06_au_22_06 = models.BooleanField("Disponible du 15-06 au 22-06")
+	dispo_22_06_au_29_06 = models.BooleanField("Disponible du 22-06 au 29-06")
+	dispo_29_06_au_06_07 = models.BooleanField("Disponible du 29-06 au 06-06")
+	dispo_06_07_au_13_07 = models.BooleanField("Disponible du 06-07 au 13-07")
+	dispo_13_07_au_20_07 = models.BooleanField("Disponible du 13-07 au 20-07")
+	dispo_20_07_au_27_07 = models.BooleanField("Disponible du 20-07 au 27-07")
+	dispo_27_07_au_03_08 = models.BooleanField("Disponible du 27-07 au 03-07")
+	dispo_03_08_au_10_08 = models.BooleanField("Disponible du 03-08 au 10-08")
+	dispo_10_08_au_17_08 = models.BooleanField("Disponible du 10-08 au 17-08")
+	dispo_17_08_au_24_08 = models.BooleanField("Disponible du 17-08 au 24-08")
+	dispo_24_08_au_31_08 = models.BooleanField("Disponible du 24-08 au 31-08")
+	dispo_31_08_au_07_09 = models.BooleanField("Disponible du 31-08 au 07-09")
+	dispo_07_09_au_14_09 = models.BooleanField("Disponible du 07-09 au 14-09")
+	dispo_14_09_au_21_09 = models.BooleanField("Disponible du 14-09 au 21-09")
+	dispo_21_09_au_28_09 = models.BooleanField("Disponible du 21-09 au 28-09")
+	dispo_28_09_au_05_10 = models.BooleanField("Disponible du 28-09 au 05-10 (centre de sauvegarde uniquement)")
+	dispo_05_10_au_12_10 = models.BooleanField("Disponible du 05-10 au 12-10 (centre de sauvegarde uniquement)")
+	dispo_12_10_au_19_10 = models.BooleanField("Disponible du 12-10 au 19-10 (centre de sauvegarde uniquement)")
+	dispo_19_10_au_26_10 = models.BooleanField("Disponible du 19-10 au 26-10 (centre de sauvegarde uniquement)")
+	dispo_26_10_au_02_11 = models.BooleanField("Disponible du 26-10 au 02-11 (centre de sauvegarde uniquement)")
 
 	duree_n_semaine = models.IntegerField("Nombre de semaines (à partir de 2)", max_length=2, blank=True)
 	ancien_surveillant = models.BooleanField("Déjà été surveillant")
@@ -96,6 +103,7 @@ class Fiche(models.Model):
 	soigneur_animalier = models.BooleanField('Soigneur animalier')
 	autre_diplome_soins = models.CharField('Autre', max_length=200, blank=True)
 	user_id = models.IntegerField("Id utilisateur", blank=True)
+	tshirt = models.CharField("T-Shirt", max_length=3, choices=CHOIX_TSHIRT, blank=True)
 
 	class Meta:
 		ordering = ['nom','prenom']
